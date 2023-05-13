1f36d61a689849260f37bc1273a571dbbbd5a431
ashhhlynn <ashleylynnwelch@gmail.com>
2023-05-13 08:43:07 -0500

    spacing

--- client/src/components/reducers/rootReducer.js
+++ client/src/components/reducers/rootReducer.js
@@ -18,78 +18,73 @@ const rootReducer = (state = initialState, action) => {
                 loading: false,
             };
 
-            case "FETCH_FAVORITES":
-                console.log(action.recipes)
-                return {
-                    ...state,
-                   favorites: action.recipes,
-                    loading: false,
-                };
+        case "FETCH_FAVORITES":
+            console.log(action.recipes)
+            return {
+                ...state,
+                favorites: action.recipes,
+                loading: false,
+            };
 
-     case "GET_RATING":
-        console.log(action.ratings)
-        let x = action.ratings.map(r => r.score)
-        const totalScores = x.reduce(
-            (previousScore, currentScore, index)=>previousScore+currentScore, 
-            0);
+        case "GET_RATING":
+            console.log(action.ratings)
+            let x = action.ratings.map(r => r.score)
+            const totalScores = x.reduce(
+                (previousScore, currentScore, index)=>previousScore+currentScore, 
+                0);
             console.log(totalScores); //returns 403
-        return {
-            ...state,
-            rating: totalScores,
-            loading: false,
-        };
+            return {
+                ...state,
+                rating: totalScores,
+                loading: false,
+            };
 
+        case "SORT_A_TO_Z":
+            console.log(...state.recipes)
+            return {
+                ...state,
+                 recipes: [...state.recipes.slice().sort((item1, item2) => item2.name < item1.name ? 1 : -1)]
+            }; 
 
-            case "SORT_A_TO_Z":
-                console.log(...state.recipes)
+        case "SORT_NUMBER_REVIEWS":
+            console.log(...state.recipes)
+            return {
+                ...state,
+                recipes: [...state.recipes.slice().sort((item1, item2) => item2.reviews.length > item1.reviews.length ? 1 : -1)]
+            }; 
+
+        case "SORT_DATE":
+            console.log(...state.recipes)
+            return {
+                ...state,
+                recipes: [...state.recipes.slice().sort((item1, item2) => item2.created_at > item1.created_at ? 1 : -1)]
+            }; 
+
+        case "SORT_RATING":
+            console.log(...state.recipes)
+            return {
+                ...state,
+                recipes: [...state.recipes.slice().sort((item1, item2) => item2.average > item1.average ? 1 : -1)]
+            }; 
+    
+        case "FILTER_INGREDIENT":
+            console.log(...state.recipes)
+            let filteredRecipes = state.recipes.filter(f => f.name === action.name)
                 return {
                     ...state,
-                    recipes: [...state.recipes.slice().sort((item1, item2) => item2.name < item1.name ? 1 : -1)]
+                    recipes:  filteredRecipes
                 }; 
-
-                case "SORT_NUMBER_REVIEWS":
-                    console.log(...state.recipes)
-                    return {
-                        ...state,
-             recipes: [...state.recipes.slice().sort((item1, item2) => item2.reviews.length > item1.reviews.length ? 1 : -1)]
-                    }; 
-
-                    case "SORT_DATE":
-                        console.log(...state.recipes)
-                        return {
-                            ...state,
-                 recipes: [...state.recipes.slice().sort((item1, item2) => item2.created_at > item1.created_at ? 1 : -1)]
-                        }; 
-
-                        case "SORT_RATING":
-                            console.log(...state.recipes)
-                            return {
-                                ...state,
-                     recipes: [...state.recipes.slice().sort((item1, item2) => item2.average > item1.average ? 1 : -1)]
-                            }; 
         
-    
-
-                        case "FILTER_INGREDIENT":
-                            console.log(...state.recipes)
-                            let filteredRecipes = state.recipes.filter(f => f.name === action.name)
-
-                            return {
-                                ...state,
-                     recipes:  filteredRecipes
-                            }; 
-        
-            case "ADD_TO_FAVORITES":
-                console.log(action.recipe)
-                if (!state.favorites.find(f=> f.id === action.recipe.id))
-                {
+        case "ADD_TO_FAVORITES":
+            console.log(action.recipe)
+            if (!state.favorites.find(f=> f.id === action.recipe.id)) {
                 return {
                     ...state,
                     favorites: [...state.favorites, action.recipe],
                     loading: false,
                 }
             }
-            else{
+            else {
                 return {
                     ...state,
                     favorites: [...state.favorites],
@@ -97,14 +92,14 @@ const rootReducer = (state = initialState, action) => {
                 }
             };
     
-                case "REMOVE_FAVORITE":
-                    console.log(action.recipe)
-                    let newFaves = state.favorites.filter(f => f.id !== action.recipe.id)
-                    return {
-                        ...state,
-                        favorites: newFaves,
-                        loading: false,
-                    };
+        case "REMOVE_FAVORITE":
+            console.log(action.recipe)
+            let newFaves = state.favorites.filter(f => f.id !== action.recipe.id)
+            return {
+                ...state,
+                favorites: newFaves,
+                loading: false,
+            };
         
         case "ADD_RECIPE":
             return {
