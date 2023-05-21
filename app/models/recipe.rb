76135d20a77f75a52e56d2374b492423a3130950
ashhhlynn <ashleylynnwelch@gmail.com>
2023-05-20 20:33:16 -0500

    validations

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,7 +4,7 @@ class Recipe < ApplicationRecord
     has_many :favorites
 
     accepts_nested_attributes_for :recipe_ingredients
-    validates :description, length: { maximum: 400 }
-    validates :name, length: { maximum: 50 }
-    validates :recipe_ingredients, length: { minimum: 0, maximum: 6 }
+    validates :description, length: { maximum: 300 }
+    validates :name, length: { maximum: 20 }
+    validates :recipe_ingredients, length: { minimum: 0, maximum: 5 }
 end
