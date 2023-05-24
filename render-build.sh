1e962157c288781fe24c685b5983f98ffd30c0ec
ashhhlynn <ashleylynnwelch@gmail.com>
2023-05-24 03:09:12 -0500

    test

--- render-build.sh
+++ render-build.sh
@@ -8,7 +8,6 @@ npm install --prefix client && npm run build --prefix client
 cp -a client/build/. public/
 
 bundle install
-bundle exec rails assets:precompile
 bundle exec rails assets:clean
 bundle exec rails db:migrate
 
